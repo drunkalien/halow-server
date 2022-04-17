322725804297832304b9089b15b969031a986155
Jamshidkhuja Burikhujaev <jamshidxojab@gmail.com>
2022-04-17 19:43:50 +0500

    fixed cors

--- server.ts
+++ server.ts
@@ -10,7 +10,7 @@ dotenv.config();
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://halow-client.vercel.app/",
     methods: ["POST", "GET"],
   },
 });
